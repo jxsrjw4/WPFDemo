ebd4218171b7911654ec60aac93c2b220f70e593
wen.jiang <jw_22@qq.com>
2020-02-24 16:49:43 +0800

    add LoginWindows and design mainwindows

---
+++ LoginViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFDemo
+{
+    public class LoginViewModel: BindableBase
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public DelegateCommand LoginInCommand { get; private set; }
+
+        public LoginViewModel()
+        {
+            LoginInCommand = new DelegateCommand(LoginIn);
+        }
+
+        public void LoginIn()
+        {
+            if (string.IsNullOrEmpty(UserName))
+            {
+            }
+            else if (string.IsNullOrEmpty(Password))
+            { 
+                
+            }
+        }
+    }
+}
